5f91665915190abc7060f3978e285c7b9afb7432
tildejustin <tildejustin@gmail.com>
2026-03-26 18:44:39 -0400

    ornithe

--- src/main/java/dev/tildejustin/mc155104fix/mixin/MouseMixin.java
+++ src/main/java/dev/tildejustin/mc155104fix/mixin/MouseMixin.java
@@ -1,18 +1,18 @@
 package dev.tildejustin.mc155104fix.mixin;
 
-import net.minecraft.client.Mouse;
+import net.minecraft.client.MouseHandler;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Mouse.class)
+@Mixin(MouseHandler.class)
 public abstract class MouseMixin {
     @Shadow
-    private boolean hasResolutionChanged;
+    private boolean ignoreFirstMove;
 
-    @Inject(method = "lockCursor", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;attackCooldown:I", opcode = Opcodes.PUTFIELD))
+    @Inject(method = "grabMouse", at = @At(value = "FIELD", target = "Lnet/minecraft/client/Minecraft;attackCooldown:I", opcode = Opcodes.PUTFIELD))
     private void setResChanged(CallbackInfo ci) {
-        this.hasResolutionChanged = true;
+        this.ignoreFirstMove = true;
     }
 }
